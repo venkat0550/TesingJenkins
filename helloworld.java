ea1c9e36f2d4a8f2522633b2a7d5175d8fffd6ce
venkat0550 <35876106+venkat0550@users.noreply.github.com>
2018-02-20 16:02:13 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+
+class Programming {
+  //constructor method
+  Programming() {
+    System.out.println("Hello World Jenkins");
+ 
+ 
+
+  }
+}
+public class helloworld
+{
+  public static void main(String[] args) {
+    Programming object = new Programming(); //creating object
+  }
+}
